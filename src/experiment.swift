407710d424ca9c8438a47472c574549c688bbf15
Mike Maraya <mike.maraya@gmail.com>
2016-03-23 16:37:00 -0400

    Simple Values

--- src/experiment.swift
+++ src/experiment.swift
@@ -1,1 +1,16 @@
+/*
+ * Copyright (c) 2016 Mike Maraya <mike[dot]maraya[at]gmail[dot]com>
+ * All rights reserved.
+ *
+ * This file is subject to the terms and conditions defined in
+ * https://github.com/mmaraya/swift/blob/master/LICENSE,
+ * which is part of this software package.
+ *
+ */
+
 print("Hello, world!")
+
+// Simple Values
+
+let explicitFloat: Float = 4
+
