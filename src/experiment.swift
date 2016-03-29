d637ed71058369132dc7a508d253a74258020899
Mike Maraya <mike.maraya@gmail.com>
2016-03-29 16:37:00 -0400

    for-in

--- src/experiment.swift
+++ src/experiment.swift
@@ -55,6 +55,24 @@ switch vegetable {
         print("That would make a good tea sandwich")
     case let x where x.hasSuffix("pepper"):
         print("Is it a spicy \(x)?")
-//    default:
-//        print("Everything tastes good in soup")
+    default:
+        print("Everything tastes good in soup")
 }
+
+let interestingNumbers = [
+    "Prime": [2, 3, 5, 7, 11, 13],
+    "Fibonacci": [1, 1, 2, 3, 5, 8],
+    "Square": [1, 4, 9, 16, 25],
+]
+var largest = 0
+var largestType = "Unknown"
+for (kind, numbers) in interestingNumbers {
+    for number in numbers {
+        if number > largest {
+            largest = number
+            largestType = kind 
+        }
+    }
+}
+print("The largest number is a \(largestType) with a value of \(largest)")
+
