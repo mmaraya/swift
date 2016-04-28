964b0c555667c241b96d4ebd6f0ce1b862a7bad7
Mike Maraya <mike.maraya@gmail.com>
2016-04-28 16:37:00 -0400

    Convert hex to base64

---
+++ src/crypto.challenge.01.swift
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2016 Mike Maraya <mike[dot]maraya[at]gmail[dot]com>
+ * All rights reserved.
+ *
+ * This file is subject to the terms and conditions defined in
+ * https://github.com/mmaraya/swift/blob/master/LICENSE,
+ * which is part of this software package.
+ *
+ */
+
+//
+// https://cryptopals.com/sets/1/challenges/1 -- Convert hex to base64
+//
+// The string
+//
+// 49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d
+//
+// Should produce:
+//
+// SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t
+//
+
