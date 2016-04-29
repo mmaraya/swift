1baffc021ed230c7e5823a1c14fa4e28889a2cf6
Mike Maraya <mike.maraya@gmail.com>
2016-04-29 16:37:00 -0400

    wrote unit test

--- src/crypto.challenge.01.swift
+++ src/crypto.challenge.01.swift
@@ -20,3 +20,28 @@
 // SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t
 //
 
+//
+// Unit test for convertHexToBase64
+//
+func testConvertHexToBase64() -> Bool {
+
+    let input = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
+    let output = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"
+    let result = convertHexToBase64(input)
+
+    if result == output {
+        return true
+    } else {
+        return false
+    }
+}
+
+//
+// Converts the supplied sequence of hexadecimal characters into a Base64 encoded string
+//
+func convertHexToBase64(name: String) -> String {
+    return "Construction in Progress"
+}
+
+print("Challenge 01 (Convert Hex to Base64)\t... \(testConvertHexToBase64() ? "passed" : "failed")")
+
