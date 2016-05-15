e248702aa75e4041f48f2d163561295304e6f88c
Mike Maraya <mike.maraya@gmail.com>
2016-05-14 21:37:00 -0400

    added program entry point

---
+++ src/main.swift
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2016 Mike Maraya <mike[dot]maraya[at]gmail[dot]com>
+ * All rights reserved.
+ *
+ * This file is subject to the terms and conditions defined in
+ * https://github.com/mmaraya/swift/blob/master/LICENSE,
+ * which is part of this software package.
+ *
+ */
+
+print("Challenge 01 (Convert Hex to Base64)\t... \(try testConvertHexToBase64() ? "passed" : "failed")")
+print("Challenge 02 (XOR Two Equal-Length Buffers)\t... \(testFixedXor() ? "passed" : "failed")")
+
