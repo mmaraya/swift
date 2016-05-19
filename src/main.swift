289bce2f022fa00188d7f7fc582eaae926db24e6
Mike Maraya <mike.maraya@gmail.com>
2016-05-18 21:37:00 -0400

    align test result messages

--- src/main.swift
+++ src/main.swift
@@ -8,6 +8,6 @@
  *
  */
 
-print("Challenge 01 (Convert Hex to Base64)\t... \(try testConvertHexToBase64() ? "passed" : "failed")")
+print("Challenge 01 (Convert Hex to Base64)\t\t... \(try testConvertHexToBase64() ? "passed" : "failed")")
 print("Challenge 02 (XOR Two Equal-Length Buffers)\t... \(testFixedXor() ? "passed" : "failed")")
 
